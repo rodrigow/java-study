da742dc9caa937d0d6ad1dc1851c3118a33148d7
Ana Carolina de Moura <131079318+anademoura@users.noreply.github.com>
2023-04-17 22:10:19 -0300

    implements create collection (#1)
    Co-authored-by: Ana de Moura <anamoura@leicester.local>

--- src/main/java/com/rodrigow/javastudy/collections/FirstCollections.java
+++ src/main/java/com/rodrigow/javastudy/collections/FirstCollections.java
@@ -1,10 +1,20 @@
 package com.rodrigow.javastudy.collections;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class FirstCollections {
     public Collection<String> createACollectionWith2StringsAnaAndRodrigo() {
+
+        Collection<String> firstCollection = new ArrayList<String>();
+        // ArrayList<String> ou List<String> pq herda = tips abaixo
+
+        firstCollection.add("Ana");
+        firstCollection.add("Rodrigo");
+
         // tips -> https://www.geeksforgeeks.org/how-to-learn-java-collections-a-complete-guide/
-        return null;
+
+        return firstCollection;
     }
 }
